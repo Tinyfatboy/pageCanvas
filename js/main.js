9325f6473a0108a988a7ed37d7a84131d6e39a65
tinyfatboy <tinyfatboy@outlook.com>
2017-08-02 21:32:59 +0800

    Add painter function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,56 @@
+$(function () {
+    var $canvasWrapper = $('.canvasWrapper')
 
+    let width = $canvasWrapper.width()
+    let height = $canvasWrapper.height()
+
+    var $canvas = $('<canvas></canvas>').attr('height', height).attr('width', width)
+    $canvas.appendTo($canvasWrapper)
+
+    $canvas.css({
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        background: '#E6E6E6'
+    })
+
+    let lastPoint = null
+    let canvas = $canvas[0]
+
+    $canvas.on('touchstart', function (e) {
+        let touch = e.originalEvent.touches[0]
+        let {x, y} = getMousePosition(canvas, touch)
+        lastPoint = {x: x, y: y}
+
+        addPoint(x, y)
+    })
+    
+    $canvas.on('touchmove', function (e) {
+        e.preventDefault()
+        let touch = e.originalEvent.touches[0]
+        let {x, y} = getMousePosition(canvas, touch)
+
+        drawLine(lastPoint.x, lastPoint.y, x, y)
+        lastPoint = {x: x, y: y}
+    })
+
+    function getMousePosition(canvas, event) {
+        let rect = canvas.getBoundingClientRect()
+        let x = event.clientX - rect.left
+        let y = event.clientY - rect.top
+        return {x, y}
+    }
+
+    function addPoint(x, y) {
+        var ctx = canvas.getContext('2d');
+        ctx.fillRect(x,y,1,1);
+    }
+
+    function drawLine(x1, y1, x2, y2){
+        var ctx = canvas.getContext('2d')
+        ctx.beginPath()
+        ctx.moveTo(x1, y1)
+        ctx.lineTo(x2, y2)
+        ctx.stroke()
+    }
+})
