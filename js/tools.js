0e8ca1f5128938e01baf5cb9f07318172a5b2446
tinyfatboy <tinyfatboy@outlook.com>
2017-08-03 19:30:20 +0800

    添加svg图标具体功能

---
+++ js/tools.js
@@ -0,0 +1,49 @@
+var $pen = $('.pen')
+var $clear = $('.clear')
+var $erase = $('.erase')
+var $painter = $('.painter')
+var $download = $('.download')
+
+var $panel = $('.panel')
+
+var panelState = 'pen'
+testPanel(panelState)
+
+$panel.on('touchstart', function (e) {
+    var targetElement = e.originalEvent.target
+    console.log(targetElement.tagName)
+    while (targetElement.tagName !== 'DIV') {
+        if (targetElement === $panel[0]) {
+            targetElement = null
+            break
+        } else {
+            targetElement = targetElement.parentNode
+        }
+    }
+
+    if (targetElement) {
+        panelState = targetElement.className
+        if (panelState !== 'download' && panelState !== 'clear') {
+            testPanel(panelState)
+        }
+        else if(panelState === 'clear'){
+            console.log($canvas)
+        }
+    }
+})
+
+function testPanel(panelState) {
+    if (panelState === 'pen') {
+        $panel.children('div').css('background', '#FFFFFF')
+        $pen.css('background', '#F074A4')
+    } else if (panelState === 'erase') {
+        $panel.children('div').css('background', '#FFFFFF')
+        $erase.css('background', '#F074A4')
+    } else if (panelState === 'painter') {
+        $panel.children('div').css('background', '#FFFFFF')
+        $painter.css('background', '#F074A4')
+    } else {
+        $panel.children('div').css('background', '#FFFFFF')
+        $pen.css('background', '#F074A4')
+    }
+}
